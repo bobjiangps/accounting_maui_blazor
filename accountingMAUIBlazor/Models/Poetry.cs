b6b40e4d73d258bb31515c4b96a164ee592184a8
Bo Jiang <bo.jiang@activenetwork.com>
2024-01-26 16:52:00 +0800

    add debug model

---
+++ accountingMAUIBlazor/Models/Poetry.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace accountingMAUIBlazor.Models;
+
+public class Poetry
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Content { get; set; }
+}
+
