16a83de5caea58518c7dbab94f6691a31968b6da
Bo Jiang <bo.jiang@activenetwork.com>
2024-01-26 16:52:26 +0800

    add debug page with code

---
+++ accountingMAUIBlazor/Pages/Poetry.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace accountingMAUIBlazor.Pages;
+
+public partial class Poetry
+{
+	private async Task InitializeAsync()
+	{
+		await ips.InitializeAsync();
+
+    }
+
+    private string _name;
+
+    private string _content;
+
+    private string _query;
+
+    private IEnumerable<Models.Poetry> _poetries = new List<Models.Poetry>();
+
+    private async Task SaveAsync()
+    {
+        //var poetry = new Models.Poetry();
+        //poetry.Name = _name;
+        //poetry.Content = _content;
+        var poetry = new Models.Poetry
+        {
+            Name = _name,
+            Content = _content
+        };
+        await ips.SavePoetryAsync(poetry);
+    }
+
+    private async Task SearchAsync()
+    {
+        _poetries =  await ips.SearchByNameAsync(_query);
+
+    }
+}
