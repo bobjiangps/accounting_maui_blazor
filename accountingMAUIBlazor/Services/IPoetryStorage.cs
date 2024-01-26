119d7a05b98bc1396f4fb560aaf174f9f62e9547
Bo Jiang <bo.jiang@activenetwork.com>
2024-01-26 16:53:19 +0800

    add debug service code

---
+++ accountingMAUIBlazor/Services/IPoetryStorage.cs
@@ -0,0 +1,15 @@
+﻿using accountingMAUIBlazor.Models;
+
+namespace accountingMAUIBlazor.Services;
+
+public interface IPoetryStorage
+{
+    Task InitializeAsync();
+
+    Task SavePoetryAsync(Poetry poetry);
+
+    Task<Poetry> GetPoetryAsync(int id);
+
+    Task<IEnumerable<Poetry>> SearchByNameAsync(string name);
+}
+
---
+++ accountingMAUIBlazor/Services/PoetryStorage.cs
@@ -0,0 +1,47 @@
+﻿using accountingMAUIBlazor.Models;
+using SQLite;
+
+namespace accountingMAUIBlazor.Services
+{
+	public class PoetryStorage: IPoetryStorage
+	{
+        public const string DBName = "poetrydb.sqlite";
+
+        public static readonly string PoetryDBPath = Path.Combine(
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData
+                ), DBName
+            );
+
+        private SQLiteAsyncConnection _connection;
+
+        private SQLiteAsyncConnection Connection =>
+            _connection ??= new SQLiteAsyncConnection(PoetryDBPath);
+
+        public async Task InitializeAsync()
+        {
+            await Connection.CreateTableAsync<Poetry>();
+        }
+
+        //LINQ
+        public async Task<Poetry> GetPoetryAsync(int id)
+        {
+            return await Connection.Table<Poetry>()
+                    .Where(p => p.Id == id)
+                    .FirstOrDefaultAsync();
+        }
+
+        public async Task SavePoetryAsync(Poetry poetry)
+        {
+            await Connection.InsertAsync(poetry);
+        }
+
+        public async Task<IEnumerable<Poetry>> SearchByNameAsync(string name)
+        {
+            return await Connection.Table<Poetry>()
+                .Where(p => p.Name.Contains(name))
+                .ToListAsync();
+        }
+    }
+}
+
