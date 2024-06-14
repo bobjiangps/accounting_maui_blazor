9be07dc91f377d4a20fd676a77780bb036fdf411
Bo Jiang <bo.jiang@activenetwork.com>
2024-06-14 14:57:16 +0800

    add simple page to debug

---
+++ accountingMAUIBlazor/Pages/Account.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace accountingMAUIBlazor.Pages;
+
+
+public partial class Account
+{
+
+    [Parameter]
+    public int? Id { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        Id = Id ?? 0;
+    }
+}
+
