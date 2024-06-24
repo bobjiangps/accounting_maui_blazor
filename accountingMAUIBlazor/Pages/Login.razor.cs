3572ab12999077caafb5f42b529eed02fa2b81f4
Bo Jiang <bo.jiang@activenetwork.com>
2024-06-24 17:33:17 +0800

    update login page to allow form and token

--- accountingMAUIBlazor/Pages/Login.razor.cs
+++ accountingMAUIBlazor/Pages/Login.razor.cs
@@ -1,20 +1,58 @@
-﻿using accountingMAUIBlazor.Services;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace accountingMAUIBlazor.Pages;
 
 public partial class Login
 {
-    //private readonly IUserService _userService;
+    public bool isLoggedIn;
+    public string loginName;
     private string token;
-    private int click_count = 0;
+    public string formMessage;
+    public string errorMessage;
+    public UserLoginViewModel LoginModel = new();
 
-    public async void LoginWithoutInput()
+    protected override void OnInitialized()
+    {
+        isLoggedIn = _userService.CheckLoggedStatus();
+        if (isLoggedIn)
+        {
+            loginName = _userService.CheckLoginName();
+        }
+    }
+
+    public async Task LoginWithInput(EditContext context)
     {
         Console.WriteLine("start to login...");
-        click_count += 1;
-        token = await _userService.GetTokenAsync();
+        token = await _userService.GetTokenAsync(LoginModel.UserName, LoginModel.Password);
         Console.WriteLine($"Token is: {token}");
-        //NavigationManager.NavigateTo("/account");
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            formMessage = "登录成功, 即将跳转";
+            await Task.Delay(500);
+            NavigationManager.NavigateTo("/accounts");
+        }
+        else
+        {
+            errorMessage = "登录失败";
+        }
+    }
+
+    public async Task SignOut()
+    {
+        Console.WriteLine("准备退出登录...");
+        await _userService.RevokeTokenAsync();
+        isLoggedIn = _userService.CheckLoggedStatus();
+        Console.WriteLine("退出登录完毕...");
     }
 }
 
+public class UserLoginViewModel
+{
+    [Required(ErrorMessage = "用户名不能为空")]
+    [StringLength(16, ErrorMessage = "用户名不应超出16个字符长度")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "密码不能为空")]
+    public string Password { get; set; }
+}
